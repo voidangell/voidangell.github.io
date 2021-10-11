7aea7bcde456ecb19a0030e6af96a14c9493bb63
Void <91305378+voidangell@users.noreply.github.com>
2021-10-10 23:56:57 -0800

    Add CSS and JS files

---
+++ AngellKnits_Java.js
@@ -0,0 +1,20 @@
+//Page Buttons//
+function homepageLoad() {
+  window.location.href = "https://www.google.com/";
+}
+function aboutUsLoad() {
+  window.location.href = "https://www.google.com/";
+}
+function patternLoad() {
+  window.location.href = "https://www.google.com/";
+}
+function oAndeLoad() {
+  window.location.href = "https://www.google.com/";
+}
+function scheduleLoad() {
+  window.location.href = "https://www.google.com";
+}
+function contactUsLoad() {
+  window.location.href = "https://www.google.com";
+}
+//copywrite date updater//
