5bb38c15688b6c7d03a1d53fba30e2c14942be7e
Void <91305378+voidangell@users.noreply.github.com>
2021-10-11 01:08:09 -0800

    Connecting .js to links

--- AngellKnits_Java.js
+++ AngellKnits_Java.js
@@ -1,20 +1,20 @@
 //Page Buttons//
 function homepageLoad() {
-  window.location.href = "https://www.google.com/";
+  window.location.href = "index.html";
 }
 function aboutUsLoad() {
-  window.location.href = "https://www.google.com/";
+  window.location.href = "About_Us.html";
 }
 function patternLoad() {
-  window.location.href = "https://www.google.com/";
+  window.location.href = "Patterns.html";
 }
 function oAndeLoad() {
-  window.location.href = "https://www.google.com/";
+  window.location.href = "OandE.html";
 }
 function scheduleLoad() {
-  window.location.href = "https://www.google.com";
+  window.location.href = "Schedule.html";
 }
 function contactUsLoad() {
-  window.location.href = "https://www.google.com";
+  window.location.href = "Contact_Us.html";
 }
 //copywrite date updater//
