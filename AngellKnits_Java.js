ad0f6a10d871a84a778a7e8bfc2bc008f4e66ec9
Void <91305378+voidangell@users.noreply.github.com>
2021-12-03 23:03:01 -0900

    Addition hidden function update
    [i]img and product[i] clickable

--- AngellKnits_Java.js
+++ AngellKnits_Java.js
@@ -60,12 +60,18 @@ window.onload = function () {
   document.querySelector(".product2").addEventListener("click", showListing2);
   document
     .querySelector(".product2Img")
+    .addEventListener("click", showListing2);
+  document
+    .querySelector(".product2Img")
     .addEventListener("mouseover", showListingA2);
   document
     .querySelector(".product2Img")
     .addEventListener("mouseout", showListingB2);
   //listing 3//
   document.querySelector(".product3").addEventListener("click", showListing3);
+  document
+    .querySelector(".product3Img")
+    .addEventListener("click", showListing3);
   document
     .querySelector(".product3Img")
     .addEventListener("mouseover", showListingA3);
@@ -76,12 +82,18 @@ window.onload = function () {
   document.querySelector(".product4").addEventListener("click", showListing4);
   document
     .querySelector(".product4Img")
+    .addEventListener("click", showListing4);
+  document
+    .querySelector(".product4Img")
     .addEventListener("mouseover", showListingA4);
   document
     .querySelector(".product4Img")
     .addEventListener("mouseout", showListingB4);
   //listing 5//
   document.querySelector(".product5").addEventListener("click", showListing5);
+  document
+    .querySelector(".product5Img")
+    .addEventListener("click", showListing5);
   document
     .querySelector(".product5Img")
     .addEventListener("mouseover", showListingA5);
@@ -93,16 +105,23 @@ window.onload = function () {
 //hidden listing - click//
 //listing1 //
 function showListing1() {
-  const productListing1 = document.querySelector(".productListing1");
-  productListing1.classList.toggle("hidden");
-  productListing1.classList.toggle("active");
+  var productListing1 = document.querySelector(".productListing1");
+  if (productListing1.classList.contains("holder") === true) {
+    productListing1.classList.toggle("active");
+    productListing1.classList.remove("holder");
+  } else {
+    productListing1.classList.toggle("hidden");
+    productListing1.classList.toggle("active");
+  }
 
   console.log(productListing1.classList);
 }
 function showListingA1() {
   const productListing1 = document.querySelector(".productListing1");
-  if (productListing1.classList.contains("hidden"))
+  if (productListing1.classList.contains("hidden")) {
     productListing1.classList.toggle("hidden");
+    productListing1.classList.add("holder");
+  }
 
   console.log(productListing1.classList);
 }
@@ -111,21 +130,29 @@ function showListingB1() {
   if (productListing1.classList.contains("active") === false) return;
   else if (productListing1.classList.contains("hidden") === false)
     productListing1.classList.toggle("hidden");
+  productListing1.classList.remove("holder");
 
   console.log(productListing1.classList);
 }
 //listing 2//
 function showListing2() {
-  const productListing2 = document.querySelector(".productListing2");
-  productListing2.classList.toggle("hidden");
-  productListing2.classList.toggle("active");
+  var productListing2 = document.querySelector(".productListing2");
+  if (productListing2.classList.contains("holder") === true) {
+    productListing2.classList.toggle("active");
+    productListing2.classList.remove("holder");
+  } else {
+    productListing2.classList.toggle("hidden");
+    productListing2.classList.toggle("active");
+  }
 
   console.log(productListing2.classList);
 }
 function showListingA2() {
   const productListing2 = document.querySelector(".productListing2");
-  if (productListing2.classList.contains("hidden"))
+  if (productListing2.classList.contains("hidden")) {
     productListing2.classList.toggle("hidden");
+    productListing2.classList.add("holder");
+  }
 
   console.log(productListing2.classList);
 }
@@ -134,21 +161,29 @@ function showListingB2() {
   if (productListing2.classList.contains("active") === false) return;
   else if (productListing2.classList.contains("hidden") === false)
     productListing2.classList.toggle("hidden");
+  productListing2.classList.remove("holder");
 
   console.log(productListing2.classList);
 }
 //listing 3//
 function showListing3() {
-  const productListing3 = document.querySelector(".productListing3");
-  productListing3.classList.toggle("hidden");
-  productListing3.classList.toggle("active");
+  var productListing3 = document.querySelector(".productListing3");
+  if (productListing3.classList.contains("holder") === true) {
+    productListing3.classList.toggle("active");
+    productListing3.classList.remove("holder");
+  } else {
+    productListing3.classList.toggle("hidden");
+    productListing3.classList.toggle("active");
+  }
 
   console.log(productListing3.classList);
 }
 function showListingA3() {
   const productListing3 = document.querySelector(".productListing3");
-  if (productListing3.classList.contains("hidden"))
+  if (productListing3.classList.contains("hidden")) {
     productListing3.classList.toggle("hidden");
+    productListing3.classList.add("holder");
+  }
 
   console.log(productListing3.classList);
 }
@@ -157,21 +192,29 @@ function showListingB3() {
   if (productListing3.classList.contains("active") === false) return;
   else if (productListing3.classList.contains("hidden") === false)
     productListing3.classList.toggle("hidden");
+  productListing3.classList.remove("holder");
 
   console.log(productListing3.classList);
 }
 //listing 4//
 function showListing4() {
-  const productListing4 = document.querySelector(".productListing4");
-  productListing4.classList.toggle("hidden");
-  productListing4.classList.toggle("active");
+  var productListing4 = document.querySelector(".productListing4");
+  if (productListing4.classList.contains("holder") === true) {
+    productListing4.classList.toggle("active");
+    productListing4.classList.remove("holder");
+  } else {
+    productListing4.classList.toggle("hidden");
+    productListing4.classList.toggle("active");
+  }
 
   console.log(productListing4.classList);
 }
 function showListingA4() {
   const productListing4 = document.querySelector(".productListing4");
-  if (productListing4.classList.contains("hidden"))
+  if (productListing4.classList.contains("hidden")) {
     productListing4.classList.toggle("hidden");
+    productListing4.classList.add("holder");
+  }
 
   console.log(productListing4.classList);
 }
@@ -180,21 +223,29 @@ function showListingB4() {
   if (productListing4.classList.contains("active") === false) return;
   else if (productListing4.classList.contains("hidden") === false)
     productListing4.classList.toggle("hidden");
+  productListing4.classList.remove("holder");
 
   console.log(productListing4.classList);
 }
 //listing 5//
 function showListing5() {
-  const productListing5 = document.querySelector(".productListing5");
-  productListing5.classList.toggle("hidden");
-  productListing5.classList.toggle("active");
+  var productListing5 = document.querySelector(".productListing5");
+  if (productListing5.classList.contains("holder") === true) {
+    productListing5.classList.toggle("active");
+    productListing5.classList.remove("holder");
+  } else {
+    productListing5.classList.toggle("hidden");
+    productListing5.classList.toggle("active");
+  }
 
   console.log(productListing5.classList);
 }
 function showListingA5() {
   const productListing5 = document.querySelector(".productListing5");
-  if (productListing5.classList.contains("hidden"))
+  if (productListing5.classList.contains("hidden")) {
     productListing5.classList.toggle("hidden");
+    productListing5.classList.add("holder");
+  }
 
   console.log(productListing5.classList);
 }
@@ -203,6 +254,7 @@ function showListingB5() {
   if (productListing5.classList.contains("active") === false) return;
   else if (productListing5.classList.contains("hidden") === false)
     productListing5.classList.toggle("hidden");
+  productListing5.classList.remove("holder");
 
   console.log(productListing5.classList);
 }
